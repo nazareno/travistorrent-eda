725252eb6d3ae363080a6631d4ef3b381c2b3bef
Nazareno Andrade <nazareno@computacao.ufcg.edu.br>
2018-03-21 12:25:01 -0300

    Ajustes durante a aula de 2018-03-21

--- code/vars_por_projeto.R
+++ code/vars_por_projeto.R
@@ -1,9 +1,10 @@
 library(tidyverse)
+library(lubridate)
 library(here)
 
 travis = read_csv("data/travistorrent_8_2_2017.csv")
 
-# Remove jobs idênticos (há muitos)
+# Remove jobs idênticos no mesmo build
 travis = travis %>% 
     select(-tr_job_id) %>% 
     group_by(gh_project_name) %>% 
@@ -12,11 +13,16 @@ travis = travis %>%
 
 projetos = travis %>% 
     group_by(gh_project_name) %>% 
-    summarize(team = max(gh_team_size), 
+    summarize(team = median(gh_team_size), 
               lang = last(gh_lang), 
-              sloc = max(gh_sloc), 
+              sloc_end = max(gh_sloc), 
+              sloc_med = median(gh_sloc),
+              activity_period = interval(ymd_hms(first(na.omit(gh_first_commit_created_at))), 
+                                         ymd_hms(last(na.omit(gh_first_commit_created_at)))) %/% months(1), # sintaxe lubridate
+              num_commits = sum(gh_num_commits_in_push),
+              commits_per_month = num_commits / activity_period,
               tests_per_kloc = max(gh_test_cases_per_kloc), 
               build_success = sum(build_successful) / n())
 
 projetos %>% 
-    write_csv(here("data/projetos.csv"))
+    write_csv(here::here("data/projetos.csv"))
